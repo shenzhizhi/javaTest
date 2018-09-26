f8483b284120d9a0177154fa4c5e7525e3901b39
shenzhi <779822420@qq.com>
2018-09-26 15:34:24 +0800

    update

--- ceshi/src/main/java/ArrayListTest.java
+++ ceshi/src/main/java/ArrayListTest.java
@@ -1,21 +1,66 @@
+import java.util.*;
+
 public class ArrayListTest {
 
 
     public static void main(String[] args) {
 
+        ArrayList<String> arrayList=new ArrayList<>();
+        arrayList.add("Aasd");
+        arrayList.add("Cc2wcc");
+        arrayList.add("Bbasdb");
         //arraylist怎么转换成数组
+        String[] aa=arrayList.toArray(new String[arrayList.size()]);
+        //数组怎么转换成arraylist
+        List<String> arrayList1= Arrays.asList(aa);
         //增加元素
+        arrayList.add("d");
         //删除元素
+        arrayList.remove("d");
         //返回2个arraylist的交集
-        //获取一个无关联的子集
+        ArrayList<String> b=new ArrayList<>();
+        b.add("a");
+//        arrayList.retainAll(b);
+        //获取一个无关联的子集,前包含，后不包含
+        List<String> son=arrayList.subList(0,1);
         //遍历arraylist的几种方法
+        /**
+         * for循环，增强for循环，迭代器
+         */
+        /*for(int i=0;i<arrayList.size();i++){
+            System.out.println(arrayList.get(i));
+        }
+        for(String s : arrayList){
+            System.out.println(s);
+        }
+        Iterator<String> iterator=arrayList.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }*/
         //使用外部比较器进行排序
-
-        //虽然这是一个线程安全的类，但是他依然无法阻止其他线程使用其他方法
+        arrayList.sort(new ComparatorTest());
+        for(String s : arrayList){
+            System.out.println(s);
+        }
+        //synchronizedList虽然一个线程安全的类，但是他依然无法阻止其他线程使用其他方法
         //所以需要在可能导致线程不安全的方法外再套上一层syncharonized方法
+        List<String> list=Collections.synchronizedList(arrayList);
+
 
 
 
     }
 
+}
+
+//外部比较器，<-1升序
+class ComparatorTest implements Comparator<String>{
+    @Override
+    public int compare(String o1, String o2) {
+        if((int)o1.charAt(0)>(int)o2.charAt(0)){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
 }
--- ceshi/src/main/java/LinkListTest.java
+++ ceshi/src/main/java/LinkListTest.java
@@ -1,12 +1,58 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class LinkListTest {
     public static void main(String[] args) {
-        //linkedlist转换成数组
-        //插入元素
-        //删除元素
-        //peek poll offer pop push分别是什么意思
 
+        //linkedlist先加进来的是队头，后加进来的是队尾
+        LinkedList<String> linkedList=new LinkedList<>();
+        linkedList.add("asd");
+        linkedList.add("qwe");
+
+        //linkedlist转换成数组，需要一个string数组作为参数，才会调用正确的toArray方法，
+        //为什么要让这个数组大小是0，因为不确定多大才能装得下，所以这个方法会自己进行比较，不够创建一个新的，所以给个0强制进行新建
+        String[] ss=linkedList.toArray(new String[0]);
+        for(String s :ss){
+            System.out.println(s);
+        }
+        //插入元素
+//        linkedList.add("asd");
+        //删除元素
+//        linkedList.remove(1);
+//        linkedList.remove("asd");
+        //peek poll offer pop push分别是什么意思
+        /**
+         * peek:返回队头
+         * poll：删除队头并返回
+         * offer：add到队尾，就是单纯的add
+         * pop：删除队头并返回
+         * push：add到队头
+         */
+        /**
+         *如何遍历linkedList，有哪些方法:4种
+         * 一般4种遍历都是：
+         * 普通for循环
+         * 增强for循环
+         * 普通Iterator
+         * ListIterator
+         */
+
+
+       /* for(int i=0;i<linkedList.size();i++){
+            System.out.println(linkedList.get(i));
+        }
+        for(String s:linkedList){
+            System.out.println(s);
+        }
+        Iterator<String> iterator=linkedList.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        ListIterator<String> listIterator=linkedList.listIterator();
+        while(listIterator.hasNext()){
+            System.out.println(listIterator.next());
+        }*/
     }
 }
