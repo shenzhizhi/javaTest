a7c8256720b3952dff6a6b2a5044b001f1f940ef
shenzhi <779822420@qq.com>
2018-09-27 16:55:10 +0800

    练习包含答案

--- ceshi/src/main/java/LinkListTest.java
+++ ceshi/src/main/java/LinkListTest.java
@@ -10,23 +10,39 @@ public class LinkListTest {
         LinkedList<String> linkedList=new LinkedList<>();
         linkedList.add("asd");
         linkedList.add("qwe");
+        linkedList.add("zxc");
 
         //linkedlist转换成数组，需要一个string数组作为参数，才会调用正确的toArray方法，
         //为什么要让这个数组大小是0，因为不确定多大才能装得下，所以这个方法会自己进行比较，不够创建一个新的，所以给个0强制进行新建
-
+        String[] strings=linkedList.toArray(new String[0]);
 
         //插入元素
-
+        linkedList.add("zxc");
         //删除元素
+        linkedList.remove(2);
+
 
         //peek poll offer pop push分别是什么意思
         /**
-         * peek:
-         * poll：
-         * offer：
-         * pop：
-         * push：
+         * peek/element:都可以返回队头，在队列为空时，peek返回null，element抛异常
+         * poll/remove/pop：都是返回队头并删除，在队列为空时，poll返回null，remove抛异常,pop跟remove是相同的实现
+         * offer/add：新增元素到队尾，当队列为空时，offer返回false，add抛异常
+         * push：新增元素到队头
          */
+        //返回队头
+        System.out.println(linkedList.peek());
+        System.out.println(linkedList.element());
+
+        //返回队头并删除
+        System.out.println(linkedList.poll());
+
+        //新增元素到队尾
+        System.out.println(linkedList.add("123"));
+        System.out.println(linkedList.offer("234"));
+
+        //新增元素到队头
+        linkedList.push("345");
+
         /**
          *如何遍历linkedList，有哪些方法:4种
          * 一般4种遍历都是：
@@ -35,7 +51,20 @@ public class LinkListTest {
          * 普通Iterator
          * ListIterator
          */
-
+        for(int i=0;i<linkedList.size();i++){
+            System.out.println(linkedList.get(i));
+        }
+        for(String s : linkedList){
+            System.out.println(s);
+        }
+        Iterator<String> iterator=linkedList.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        ListIterator<String> listIterator=linkedList.listIterator();
+        while(listIterator.hasNext()){
+            System.out.println(listIterator.next());
+        }
 
     }
 }
--- ceshi/src/main/java/StringTest.java
+++ ceshi/src/main/java/StringTest.java
@@ -3,69 +3,56 @@ public class StringTest {
 
         String ss=new String("asdasd");
 
-
-
         //获取字符串长度
-        System.out.print("字符串长度：");
-
+        ss.length();
 
         //判断字符串是否为空
-        System.out.print("字符串是否为空");
-
+        ss.isEmpty();
 
         //判断字符串是否为null
-        System.out.print("字符串是否为null");
+        boolean b=ss==null;
 
 
         //获得某个下标的元素
-        System.out.print("获得某个下标的元素");
-
+        char c=ss.charAt(0);
 
         //获得某下标所在的元素的ascii
-        System.out.print("获得某下标所在的元素的ascii");
+        int ascii=ss.codePointAt(0);
 
-
-        //获得字符串的某段char数组
-        System.out.print("获得字符串的某段char数组");
-
+        //获得字符串的某段char数组??,参数顺序
+        char[] chars=new char[10];
+        ss.getChars(0,3,chars,0);
 
         // 获取字符串的bytes数组
-        System.out.print("获取字符串的bytes数组");
-
+        byte[] bytes=ss.getBytes();
         // 判断2个字符串是否相等 ??
         //比较引用
-        System.out.print("判断2个字符串是否相等");
-
+        boolean b1=ss=="asd";
 
         //判断2个字符串的值是否相等 ??
         //比较值
-        System.out.print("判断2个字符串的值是否相等");
-
+        boolean b2=ss.equals("asd");
 
         //连接字符串，不使用+符号
-        System.out.print("连接字符串，不使用+符号");
-
+        String s=ss.concat("123");
 
         //判断字符串是否匹配正则表达式
-        System.out.print("判断字符串是否匹配正则表达式");
-
+        boolean b3=ss.matches("//");
 
         //将旧字符代替为新字符
-        System.out.print("将旧字符代替为新字符");
-
+        String s1=ss.replace('a','b');
 
         //判断字符串是否包含某元素
         //错误：参数是string
-        System.out.print("判断字符串是否包含某元素");
-
+        boolean b4=ss.contains("asd");
 
         //以正则表达式分割字符串
-        System.out.print("以正则表达式分割字符串");
-        ;
+        String[] strings=ss.split("//");
 
         //将某个元素插入到字符串数组中，并合成一个新字符串
         //第一个参数是分隔符，第二个是被插的string数组
-        System.out.print("将某个元素插入到字符串数组中，并合成一个新字符串");
-
+        String[] strings1={"asd","asd","asd"};
+        String s2=String .join("=",strings1);
+        System.out.println(s2);
     }
 }
